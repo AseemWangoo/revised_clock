52d8e10a96e7a9042ae0193300c8e1ede463efe1
Aseem Wangoo <aseemwangoo@gmail.com>
2021-04-22 17:53:10 +0800

    [C/Mode] Clock modes

--- lib/home/view/home.view.dart
+++ lib/home/view/home.view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:revised_clock/home/components/button/button.component.dart';
 import 'package:revised_clock/home/components/faces/clock_face.component.dart';
+import 'package:revised_clock/home/view_models/clock_mode.viewmodel.dart';
 import 'package:revised_clock/home/view_models/time.viewmodel.dart';
 import 'package:revised_clock/locator.dart';
 
@@ -9,22 +11,45 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final clockModeVM = locator<ClockModeViewModel>();
     final timeViewModel = locator<TimeViewModel>();
 
     return Scaffold(
       appBar: AppBar(title: const Text('Clock')),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            AnimatedBuilder(
-              animation: timeViewModel,
-              builder: (context, _) {
-                return Text(timeViewModel.currentTime);
-              },
-            ),
-            const ClockFaceComponent()
-          ],
+        child: ValueListenableBuilder<ClockMode>(
+          valueListenable: clockModeVM,
+          builder: (context, value, child) {
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                if (value == ClockMode.digital)
+                  AnimatedBuilder(
+                    animation: timeViewModel,
+                    builder: (context, _) {
+                      return Text(
+                        timeViewModel.currentTime,
+                        style: Theme.of(context).textTheme.headline4,
+                      );
+                    },
+                  )
+                else
+                  const ClockFaceComponent(),
+                child,
+              ],
+            );
+          },
+          child: ButtonComponent(
+            onPressed: () {
+              final currVal = clockModeVM.value;
+
+              if (currVal == ClockMode.analog) {
+                clockModeVM.value = ClockMode.digital;
+              } else {
+                clockModeVM.value = ClockMode.analog;
+              }
+            },
+          ),
         ),
       ),
     );
--- lib/locator.dart
+++ lib/locator.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
-import 'package:revised_clock/home/view_models/time.viewmodel.dart';
 
+import 'home/view_models/clock_mode.viewmodel.dart';
+import 'home/view_models/time.viewmodel.dart';
 import 'shared/services/navigation.service.dart';
 
 final GetIt locator = GetIt.instance;
@@ -8,5 +9,6 @@ final GetIt locator = GetIt.instance;
 void setupLocator() {
   locator.registerLazySingleton(() => NavigationService());
 
+  locator.registerLazySingleton(() => ClockModeViewModel());
   locator.registerLazySingleton(() => TimeViewModel());
 }
